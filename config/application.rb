aa928a31dbf9a3329dea1554c414936704b2e96b
Mark Greene <mark.greene@yahoo.com>
2014-07-16 11:09:56 -0400

    Iteration H1

--- config/application.rb
+++ config/application.rb
@@ -20,17 +20,14 @@ module Depot
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.default_url_options = { :host => "localhost:3000" }
-
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
       domain: "gmail.com",
       authentication: "plain",
-      user_name: "mbg.depot@gmail.com",
-      password: ENV["PASSWORD"],
+      user_name: ENV["DEPOT_EMAIL"],
+      password: ENV["DEPOT_PASSWORD"],
       enable_starttls_auto: true
     }
 
