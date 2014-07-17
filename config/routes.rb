e6dd5bb5babad5b82eceedfa33929eb4ea738c6c
Mark Greene <mark.greene@yahoo.com>
2014-07-16 22:28:55 -0400

    Iteration J4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
+
   controller :sessions do
     get  'login' => :new
     post 'login' => :create
@@ -8,14 +9,18 @@ Rails.application.routes.draw do
 
   get "sessions/create"
   get "sessions/destroy"
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
 
-  get 'store/index'
+  resources :users
+
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -23,7 +28,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
