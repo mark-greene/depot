f19a6e1e65b6863f1b52cb4bef2bd5eb9cd9510e
Mark Greene <mark.greene@yahoo.com>
2014-07-17 12:29:22 -0400

    Heroku

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Depot
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.enforce_available_locales = true
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
